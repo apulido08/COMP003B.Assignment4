be2fb477bc33d231430339d7729daddce63f1c25
Alvaro Pulido <alavropulido@my.whccd.edu>
2025-04-11 20:09:52 -0700

    Add project files.

---
+++ COMP003B.Assignment4/Controllers/VideoGameController.cs
@@ -0,0 +1,28 @@
+﻿using COMP003B.Assignment4.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+
+namespace COMP003B.Assignment4.Controllers
+{
+    public class VideoGamesController : Controller
+    {
+        public IActionResult Index()
+        {
+            var videoGames = new List<VideoGame>
+        {
+            new VideoGame { Id = 1, Title = "Quest of Legends", Genre = "RPG", Description = "Embark on epic quests in a magical realm.", ReleaseDate = new DateTime(2022, 5, 15) },
+            new VideoGame { Id = 2, Title = "Pixel Racer", Genre = "Racing", Description = "Race through pixel-perfect tracks.", ReleaseDate = new DateTime(2023, 3, 1) }
+        };
+
+            return View(videoGames);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var videoGame = new VideoGame { Id = id, Title = $"Game {id}", Genre = "Unknown", Description = "Detailed description coming soon.", ReleaseDate = DateTime.Now };
+            return View(videoGame);
+        }
+    }
+
+}
---
+++ COMP003B.Assignment4/Models/VideoGame.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Data;
+
+namespace COMP003B.Assignment4.Models
+{
+    public class VideoGame
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; }
+
+        [Required]
+        [StringLength(300)]
+        public string Description { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Genre { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+    }
+}
